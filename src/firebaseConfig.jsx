79e1b6228c1120bcff738a6786f43d9846d803fd
Takalani Nedzanani <135524369+Takalani-Nedzanani@users.noreply.github.com>
2025-04-24 23:22:36 +0200

    new lines added

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -6,13 +6,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCJqBmSimLnjnafNoWNFNjOoO-cILG8cWs",
-    authDomain: "africa-real-estate.firebaseapp.com",
-    projectId: "africa-real-estate",
-    storageBucket: "africa-real-estate.firebasestorage.app",
-    messagingSenderId: "886722366229",
-    appId: "1:886722366229:web:65b291e67137c90f3fb71c",
-    measurementId: "G-H4XNX8DR4H"
+    apiKey: "your api key",
+    authDomain: "your auth domain",
+    projectId: "your project id",
+    storageBucket: "your storage bucket",
+    messagingSenderId: "your messaging sender id",
+    appId: "your app id",
+    measurementId: "your measurement id",
 };
 
 const app = initializeApp(firebaseConfig);
